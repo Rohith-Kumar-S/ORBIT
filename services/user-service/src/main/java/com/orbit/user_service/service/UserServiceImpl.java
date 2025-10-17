bed5d6a3cfdb8fdd11645fa15b6e60d2b4d88874
Rohith S <rohithkumarvsk@gmail.com>
2025-10-17 00:06:04 -0400

    fix response

--- services/user-service/src/main/java/com/orbit/user_service/service/UserServiceImpl.java
+++ services/user-service/src/main/java/com/orbit/user_service/service/UserServiceImpl.java
@@ -14,14 +14,15 @@ import com.orbit.user_service.dto.UserDetails;
 import jakarta.transaction.Transactional;
 
 @Service
-public class UserServiceImpl implements UserServiceInterface{
-	
+public class UserServiceImpl implements UserServiceInterface {
+
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final JWTTokenService jwtTokenService;
-	
+
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JWTTokenService jwtTokenService) {
+	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder,
+			JWTTokenService jwtTokenService) {
 		// TODO Auto-generated constructor stub
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
@@ -39,32 +40,30 @@ public class UserServiceImpl implements UserServiceInterface{
 		model_user.setMobileNumber(user.getMobileNumber());
 		model_user.setRole(user.getRole());
 		this.userRepository.save(model_user);
-		
-		Optional<com.orbit.user_service.model.User> fetcheduser= this.userRepository.findById(model_user.getId());
-		
-		if(fetcheduser.isPresent()) {
+
+		Optional<com.orbit.user_service.model.User> fetcheduser = this.userRepository.findById(model_user.getId());
+
+		if (fetcheduser.isPresent()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
-		
+
 	}
 
 	@Override
 	public LoginResponse loginUser(User user) {
 		// TODO Auto-generated method stub
-		
-		Optional<com.orbit.user_service.model.User> modaluser= this.userRepository.findByName(user.getName());
-		
-		if(modaluser.isPresent()) {
-			String token = jwtTokenService.generateToken(modaluser.get());
-			return new LoginResponse(Boolean.TRUE, token);
-			
+
+		Optional<com.orbit.user_service.model.User> modaluser = this.userRepository.findByName(user.getName());
+
+		if (modaluser.isPresent()) {
+			if (passwordEncoder.matches(user.getPassword(), modaluser.get().getPassword())) {
+				String token = jwtTokenService.generateToken(modaluser.get());
+				return new LoginResponse(Boolean.TRUE, token);
+			}
 		}
 		return new LoginResponse(Boolean.FALSE, "");
 	}
-	
-	
 
 }
